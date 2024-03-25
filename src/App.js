509a360450e31cef4775ea3bb9fe4801c35d24b9
marik <letschuka@gmail.com>
2024-03-25 13:23:20 +0200

    add routing: home and settings

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import { Header } from './components/Header/Header';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {Home} from './pages/Home/Home';
+import {Settings} from './pages/Settings/Settings';
 
 export function App() {
 
   return (
     <>
-      <CssBaseline />
-      <Header />
+      <BrowserRouter>
+        <CssBaseline />
+        <Header />
+
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='settings' element={<Settings />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
